878713797de95f3f4f3c5cf6d70bb3fe3bf70571
Henrique <ham2@cin.ufpe.br>
2020-10-29 18:43:04 -0300

    Adding Tilesets and Putting Levels in a Folder

---
+++ Tileset/Image Colection/ObjectsCollection.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="ObjectsCollection" tilewidth="80" tileheight="60" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2" type="player">
+  <image width="48" height="32" source="player.png"/>
+ </tile>
+ <tile id="4" type="heatPlace">
+  <image width="32" height="32" source="heatPlace.png"/>
+ </tile>
+ <tile id="5" type="enemyBasic">
+  <image width="28" height="30" source="enemyGoblin.png"/>
+ </tile>
+ <tile id="6" type="finishPlace">
+  <image width="36" height="60" source="FinishPlace.png"/>
+ </tile>
+ <tile id="7" type="movingPlataform">
+  <image width="80" height="32" source="movingPlataform.png"/>
+ </tile>
+ <tile id="10" type="fallingPlataform">
+  <image width="80" height="32" source="fallingPlataform.png"/>
+ </tile>
+ <tile id="11" type="oneWayPlataform">
+  <image width="48" height="8" source="oneWayPlataform.png"/>
+ </tile>
+</tileset>
---
+++ Tileset/MixAndJam/MixAndJam3.tsx
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="MixAndJam3" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="MixJam_Tileset.png" trans="000000" width="256" height="256"/>
+ <terraintypes>
+  <terrain name="Ground" tile="68"/>
+  <terrain name="Wall" tile="73"/>
+  <terrain name="Wall 2" tile="73"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,"/>
+ <tile id="1" terrain="0,0,,"/>
+ <tile id="2" terrain="0,0,,0"/>
+ <tile id="16" terrain="0,,0,"/>
+ <tile id="18" terrain=",0,,0"/>
+ <tile id="32" terrain="0,,0,"/>
+ <tile id="34" terrain=",0,,0"/>
+ <tile id="48" terrain="0,,0,0"/>
+ <tile id="49" terrain=",,0,0"/>
+ <tile id="50" terrain=",0,0,0"/>
+ <tile id="66" terrain="1,1,1,1"/>
+ <tile id="67" terrain="1,1,1,1"/>
+ <tile id="68" terrain="1,1,1,1"/>
+ <tile id="69" terrain="1,1,1,1"/>
+ <tile id="70" terrain="1,1,1,1"/>
+ <tile id="71" terrain="2,2,2,"/>
+ <tile id="72" terrain="2,2,,"/>
+ <tile id="73" terrain="2,2,,"/>
+ <tile id="74" terrain="2,2,,"/>
+ <tile id="75" terrain="2,2,,2"/>
+ <tile id="82" terrain="1,1,1,1"/>
+ <tile id="83" terrain="1,1,1,1"/>
+ <tile id="84" terrain="1,1,1,1"/>
+ <tile id="85" terrain="1,1,1,1"/>
+ <tile id="86" terrain="1,1,1,1"/>
+ <tile id="87" terrain="2,,2,"/>
+ <tile id="91" terrain=",2,,2"/>
+ <tile id="98" terrain="1,1,1,1"/>
+ <tile id="99" terrain="1,1,1,1"/>
+ <tile id="100" terrain="1,1,1,1"/>
+ <tile id="101" terrain="1,1,1,1"/>
+ <tile id="102" terrain="1,1,1,1"/>
+ <tile id="103" terrain="2,,2,"/>
+ <tile id="107" terrain=",2,,2"/>
+ <tile id="114" terrain="1,1,1,1"/>
+ <tile id="115" terrain="1,1,1,1"/>
+ <tile id="116" terrain="1,1,1,1"/>
+ <tile id="117" terrain="1,1,1,1"/>
+ <tile id="118" terrain="1,1,1,1"/>
+ <tile id="119" terrain="2,,2,2"/>
+ <tile id="120" terrain=",,2,2"/>
+ <tile id="121" terrain=",,2,2"/>
+ <tile id="122" terrain=",,2,2"/>
+ <tile id="123" terrain=",2,2,2"/>
+</tileset>
