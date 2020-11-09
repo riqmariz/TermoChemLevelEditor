0ac90d87ea08ec142501e618e3efb0a975df48fe
Henrique <ham2@cin.ufpe.br>
2020-11-09 18:14:45 -0300

    adding new tilesets

---
+++ Tileset/TilesetDeserto/desertoTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="desertoTileset" tilewidth="191" tileheight="191" tilecount="9" columns="3">
+ <image source="desertoTileset.png" trans="000000" width="573" height="573"/>
+</tileset>
---
+++ Tileset/TilesetPantano/pantanoTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="pantanoTileset" tilewidth="191" tileheight="191" tilecount="16" columns="4">
+ <image source="pantanoTileset.png" trans="000000" width="764" height="764"/>
+</tileset>
