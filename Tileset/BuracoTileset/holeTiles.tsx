ec781db04599f441acd0e0379bc36872fe979e25
Henrique <ham2@cin.ufpe.br>
2020-11-12 17:44:25 -0300

    Buraco tileset added

---
+++ Tileset/BuracoTileset/holeTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="holeTiles" tilewidth="191" tileheight="191" tilecount="1" columns="1">
+ <image source="holeTiles.png" trans="1dff00" width="191" height="191"/>
+</tileset>
