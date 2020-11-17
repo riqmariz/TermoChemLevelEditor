dfa1b1acf3e91d23c6841d042aa7a0452717fc0a
Henrique <ham2@cin.ufpe.br>
2020-11-17 13:13:08 -0300

    background deserto added to backgrounds

--- Tileset/Backgrounds/backgrounds.tsx
+++ Tileset/Backgrounds/backgrounds.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="backgrounds" tilewidth="5535" tileheight="4202" tilecount="1" columns="0">
+<tileset version="1.4" tiledversion="1.4.2" name="backgrounds" tilewidth="5638" tileheight="4202" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="backgroundTerra">
   <image width="5535" height="4202" source="BG_terra.png"/>
+ </tile>
+ <tile id="1" type="backgroundDeserto">
+  <image width="5638" height="3545" source="BG_deserto.png"/>
  </tile>
 </tileset>
