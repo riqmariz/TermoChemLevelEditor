19054049c0a6086bd4495c6fe3ffb1f202750f8c
Henrique <ham2@cin.ufpe.br>
2020-11-17 13:05:48 -0300

    tileset agua deserto

---
+++ Tileset/TilesetAguaDeserto/desertoAguaTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="desertoAguaTileset" tilewidth="191" tileheight="191" tilecount="6" columns="3">
+ <image source="desertoAguaTileset.png" trans="000000" width="573" height="382"/>
+</tileset>
