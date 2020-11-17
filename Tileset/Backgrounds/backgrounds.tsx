bd387c75ab1bd8265c44bb9f7a8bd13f3c175c9a
Henrique <ham2@cin.ufpe.br>
2020-11-17 16:13:55 -0300

    Pantano Props + Background Pantano added

--- Tileset/Backgrounds/backgrounds.tsx
+++ Tileset/Backgrounds/backgrounds.tsx
@@ -1,10 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="backgrounds" tilewidth="5638" tileheight="4202" tilecount="2" columns="0">
+<tileset version="1.4" tiledversion="1.4.2" name="backgrounds" tilewidth="5638" tileheight="4202" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="backgroundTerra">
   <image width="5535" height="4202" source="BG_terra.png"/>
  </tile>
  <tile id="1" type="backgroundDeserto">
   <image width="5638" height="3545" source="BG_deserto.png"/>
+ </tile>
+ <tile id="2" type="backgroundPantano">
+  <image width="5638" height="3545" source="bg.png"/>
  </tile>
 </tileset>
---
+++ Tileset/PropsPantano/PropsPantanoObjects.tsx
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="PropsPantanoObjects" tilewidth="808" tileheight="2301" tilecount="22" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="algas1">
+  <image width="174" height="381" source="algas.png"/>
+ </tile>
+ <tile id="1" type="algas2">
+  <image width="240" height="237" source="algas_02.png"/>
+ </tile>
+ <tile id="2" type="algas2Esq">
+  <image width="240" height="237" source="algas_02_esq.png"/>
+ </tile>
+ <tile id="3" type="algas1Esq">
+  <image width="174" height="381" source="algas_esq.png"/>
+ </tile>
+ <tile id="4" type="arbusto1">
+  <image width="305" height="325" source="arbusto_01.png"/>
+ </tile>
+ <tile id="5" type="arbusto2">
+  <image width="254" height="270" source="arbusto_02.png"/>
+ </tile>
+ <tile id="6" type="arvoreFundo1">
+  <image width="428" height="2223" source="arvore_fundo_1.png"/>
+ </tile>
+ <tile id="7" type="arvoreFundo2">
+  <image width="241" height="2224" source="arvore_fundo_2.png"/>
+ </tile>
+ <tile id="8" type="arvoreFundo3">
+  <image width="180" height="2258" source="arvore_fundo_3.png"/>
+ </tile>
+ <tile id="9" type="arvoreFundo4">
+  <image width="428" height="2224" source="arvore_fundo_4.png"/>
+ </tile>
+ <tile id="10" type="arvoreFundo5">
+  <image width="180" height="2258" source="arvore_fundo_5.png"/>
+ </tile>
+ <tile id="11" type="arvoreFundo6">
+  <image width="241" height="2224" source="arvore_fundo_6.png"/>
+ </tile>
+ <tile id="12" type="arvoreTerceiroPlano1">
+  <image width="475" height="2301" source="arvore_terceiro_plano_1.png"/>
+ </tile>
+ <tile id="13" type="arvoreTerceiroPlano2">
+  <image width="486" height="2252" source="arvore_terceiro_plano_2.png"/>
+ </tile>
+ <tile id="14" type="arvoreTerceiroPlano3">
+  <image width="448" height="2200" source="arvore_terceiro_plano_3.png"/>
+ </tile>
+ <tile id="15" type="arvoreTerceiroPlano4">
+  <image width="475" height="2301" source="arvore_terceiro_plano_4.png"/>
+ </tile>
+ <tile id="16" type="arvoreTerceiroPlano5">
+  <image width="486" height="2252" source="arvore_terceiro_plano_5.png"/>
+ </tile>
+ <tile id="17" type="arvoreTerceiroPlano6">
+  <image width="448" height="2200" source="arvore_terceiro_plano_6.png"/>
+ </tile>
+ <tile id="18" type="copaFrente">
+  <image width="808" height="1612" source="copa_frente.png"/>
+ </tile>
+ <tile id="19" type="copaFundo">
+  <image width="608" height="1188" source="copa_fundo.png"/>
+ </tile>
+ <tile id="20" type="copaPrimeiroPlano1">
+  <image width="765" height="1137" source="copa_primeiro_plano_01.png"/>
+ </tile>
+ <tile id="21" type="copaPrimeiroPlano2">
+  <image width="304" height="997" source="copa_primeiro_plano_02.png"/>
+ </tile>
+</tileset>
