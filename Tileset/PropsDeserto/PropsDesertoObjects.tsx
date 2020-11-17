26a7fd3da1370e91fb77a680f1eaca6baf17522c
Henrique <ham2@cin.ufpe.br>
2020-11-17 15:36:09 -0300

    PropsDeserto added

---
+++ Tileset/PropsDeserto/PropsDesertoObjects.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="PropsDesertoObjects" tilewidth="1900" tileheight="1247" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="cacto1">
+  <image width="144" height="501" source="cacto_1.png"/>
+ </tile>
+ <tile id="1" type="cacto2">
+  <image width="145" height="501" source="cacto_2.png"/>
+ </tile>
+ <tile id="2" type="cacto3">
+  <image width="61" height="248" source="cacto_3.png"/>
+ </tile>
+ <tile id="3" type="cacto4">
+  <image width="144" height="357" source="cacto_4.png"/>
+ </tile>
+ <tile id="4" type="cacto5">
+  <image width="144" height="358" source="cacto_5.png"/>
+ </tile>
+ <tile id="5" type="coqueiro">
+  <image width="839" height="1223" source="coqueiro.PNG"/>
+ </tile>
+ <tile id="6" type="coqueiroEsq">
+  <image width="458" height="668" source="coqueiro_esq.png"/>
+ </tile>
+ <tile id="7" type="luzSol">
+  <image width="700" height="1047" source="luz_sol.png"/>
+ </tile>
+ <tile id="8" type="segundoPlano">
+  <image width="1900" height="1246" source="segundo_plano.png"/>
+ </tile>
+ <tile id="9" type="segundoPlanoEsq">
+  <image width="1900" height="1247" source="segundo_plano_esq.png"/>
+ </tile>
+ <tile id="10" type="sol">
+  <image width="373" height="373" source="sol.png"/>
+ </tile>
+ <tile id="11" type="terceiroPlano">
+  <image width="1171" height="890" source="terceiro_plano.png"/>
+ </tile>
+ <tile id="12" type="terceiroPlanoEsq">
+  <image width="1172" height="890" source="terceiro_plano_esq.png"/>
+ </tile>
+</tileset>
