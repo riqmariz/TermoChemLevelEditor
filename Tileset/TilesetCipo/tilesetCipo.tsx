b02e4a1cbeba1d46353d58eb9e08aa3c574283a7
Henrique <ham2@cin.ufpe.br>
2020-11-20 11:42:47 -0300

    cipo tileset added + new Vine Tile Layer

---
+++ Tileset/TilesetCipo/tilesetCipo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tilesetCipo" tilewidth="191" tileheight="191" tilecount="1" columns="1">
+ <image source="tilesetCipo.png" trans="000000" width="191" height="191"/>
+</tileset>
