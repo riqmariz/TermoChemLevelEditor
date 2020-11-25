4edb04c00269b4f23aed9bdd781c2594217b10a8
Henrique <ham2@cin.ufpe.br>
2020-11-25 14:20:18 -0300

    adding new backgrounds and new over background ice tileset

--- Tileset/Backgrounds/backgrounds.tsx
+++ Tileset/Backgrounds/backgrounds.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="backgrounds" tilewidth="5638" tileheight="4202" tilecount="3" columns="0">
+<tileset version="1.4" tiledversion="1.4.2" name="backgrounds" tilewidth="5638" tileheight="4202" tilecount="8" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="backgroundTerra">
   <image width="5535" height="4202" source="BG_terra.png"/>
@@ -9,5 +9,20 @@
  </tile>
  <tile id="2" type="backgroundPantano">
   <image width="5638" height="3545" source="bg.png"/>
+ </tile>
+ <tile id="3" type="backgroundHighDeserto">
+  <image width="5638" height="3546" source="BG_alto_deserto.png"/>
+ </tile>
+ <tile id="4" type="backgroundHighPantano">
+  <image width="5638" height="3549" source="BG_alto_pantano.png"/>
+ </tile>
+ <tile id="5" type="backgroundLowDeserto">
+  <image width="5638" height="3546" source="BG_baixo_deserto.png"/>
+ </tile>
+ <tile id="6" type="backgroundLowPantano">
+  <image width="5638" height="3549" source="BG_baixo_pantano.png"/>
+ </tile>
+ <tile id="7" type="backgroundPantano">
+  <image width="5638" height="3545" source="bg_pantano.png"/>
  </tile>
 </tileset>
---
+++ Tileset/OverBackgroundIceTileset/overBackgroundTilesetGelo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="overBackgroundTilesetGelo" tilewidth="191" tileheight="191" tilecount="4" columns="2">
+ <image source="overBackgroundTilesetGelo.png" trans="000000" width="382" height="382"/>
+</tileset>
